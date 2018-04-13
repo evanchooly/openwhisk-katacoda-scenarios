2ae6245b1dbf8c694f1a83351289a5a02133b35d
Justin Lee <julee@redhat.com>
2018-04-13 11:35:35 -0400

    user set-env.sh to set things up

--- chained-actions/env-init.sh
+++ chained-actions/env-init.sh
@@ -1,5 +1,1 @@
-ssh root@host01 <<EOF
-yum -y install wget tree
-until $(oc status &> /dev/null); do sleep 1; done; oc adm policy add-cluster-role-to-user cluster-admin admin
-oc new-project faas --display-name="FaaS - Apache OpenWhisk"
-EOF
+ssh root@host01 "yum -y install wget tree"
